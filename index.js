8b9d24eb1522489c6593c2949fba500bfa6f1dd5
TomKALBE <t.kalbe@h3hitema.fr>
2022-04-29 12:22:13 +0200

    Event listener with callback who handle number%5 and event emit

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const events = require('events');
 // Create an eventEmitter object
 const eventEmitter = new events.EventEmitter();
 
+const callBack = (number) => {
+   if(number % 5 === 0)
+      console.log("Buzz")
+}
+
+// Add Event Listeners
+eventEmitter.addListener('FizzBuzz', callBack);
+
+// Fire event
+eventEmitter.emit('FizzBuzz', 5);
